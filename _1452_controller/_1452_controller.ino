91cb2ba02e811957e781b431ef0a3a6daa229219
tecteun <tecteun@hotmail.com>
2019-01-08 21:13:30 +0100

    add m5stack support though broken, see https://github.com/me-no-dev/AsyncTCP/pull/30

--- _1452_controller/_1452_controller.ino
+++ _1452_controller/_1452_controller.ino
@@ -1,9 +1,13 @@
+
+//#define M5STACK
+
+#if defined(M5STACK)
+#include <M5Stack.h>
+#endif
+
 #include <DNSServer.h>
 #include <ESPUI.h>
 #include <SPI.h>
-const byte DNS_PORT = 53;
-IPAddress apIP(192, 168, 1, 1);
-DNSServer dnsServer;
 
 #if defined(ESP32)
 #include <WiFi.h>
@@ -11,7 +15,7 @@ DNSServer dnsServer;
 #include <ESP8266WiFi.h>
 #endif
 
-const char *ssid = "1452";
+const char *ssid = "";
 const char *password = "";
 
 long oldTime = 0;
@@ -21,34 +25,59 @@ void numberCall(Control sender, int type) { Serial.println(sender.value); }
 
 void textCall(Control sender, int type) { Serial.println(sender.value); }
 
-void slider(Control sender, int type) { Serial.println(sender.value); }
+void slider(Control sender, int type) { 
+  
+  #if defined(M5STACK)
+  M5.Lcd.setCursor(50, 50, 2);
+  M5.Lcd.print(sender.value); 
+  #endif
+  Serial.println(sender.value); 
+}
+
+#if defined(M5STACK)
+int SS_PIN = SCL;//D0;
+#else
+int SS_PIN = D0;
+#endif
+
+
+union stuff
+{
+   int a;
+   byte b[4];
+};
 
 void buttonCallback(Control sender, int type) {
+  SPI.beginTransaction(SPISettings(22000000, MSBFIRST, SPI_MODE3));
   switch (type) {
     case B_DOWN:
       {
         Serial.println("Button DOWN");
-        digitalWrite(D0, LOW);
+        digitalWrite(SS_PIN, LOW);
         SPI.transfer(0b00000000); //== write //0x00000001 == read
         SPI.transfer16(0x73FF);
+        //8.24 number format
+        //multiply by 2^24
+        stuff sendThis;
+        sendThis.a = 1 * (2^24) ;
         byte cmd[4]={0x01,0x00,0x00,0x00};
         SPI.transfer(cmd, 4);
-        digitalWrite(D0, HIGH);
+        digitalWrite(SS_PIN, HIGH);
       }
       break;
     case B_UP:
       {
         Serial.println("Button UP");
-        Serial.println("Button DOWN");
-        digitalWrite(D0, LOW);
+        digitalWrite(SS_PIN, LOW);
         SPI.transfer(0b00000000); //== write //0x00000001 == read
         SPI.transfer16(0x73FF);
         byte cmd[4]={0x00,0x00,0x06,0x8e};
         SPI.transfer(cmd, 4);
-        digitalWrite(D0, HIGH);
+        digitalWrite(SS_PIN, HIGH);
       }
       break;
   }
+  SPI.endTransaction();
 }
 
 void buttonExample(Control sender, int type) {
@@ -127,45 +156,38 @@ void otherSwitchExample(Control sender, int value) {
 }
 
 void setup(void) {
-  pinMode(D0, OUTPUT);
-  digitalWrite(D0, HIGH); 
-  digitalWrite(D0, LOW);    
-  delayMicroseconds(1);    
-  digitalWrite(D0, HIGH);    
-  digitalWrite(D0, LOW);    
-  delayMicroseconds(1);    
-  digitalWrite(D0, HIGH);    
-  digitalWrite(D0, LOW);    
-  delayMicroseconds(1);    
-  digitalWrite(D0, HIGH);
-  digitalWrite(D0, LOW);
-  delayMicroseconds(1);    
-  digitalWrite(D0, HIGH);
-  SPI.begin();
-  SPI.beginTransaction(SPISettings(22000000, MSBFIRST, SPI_MODE3));
-  
+  #if defined(M5STACK)
+  // Initialize the M5Stack object
+  M5.begin();
 
-  
-  Serial.begin(115200);
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  /*
-  #if defined(ESP32)
-    WiFi.setHostname(ssid);
-  #else
-    WiFi.hostname(ssid);
+  // LCD display
+  M5.Lcd.println("Testing ADAu1452");  
   #endif
-  */
-/*
-  WiFi.softAP(ssid);
-  // WiFi.softAP(ssid, password);
-  Serial.println("");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.softAPIP());
-*/
-  // change the beginning to this if you want to join an existing network
-  
-     Serial.begin(115200);
+
+
+  pinMode(SS_PIN, OUTPUT);
+  digitalWrite(SS_PIN, HIGH); 
+  digitalWrite(SS_PIN, LOW);    
+  delayMicroseconds(1);    
+  digitalWrite(SS_PIN, HIGH);    
+  digitalWrite(SS_PIN, LOW);    
+  delayMicroseconds(1);    
+  digitalWrite(SS_PIN, HIGH);    
+  digitalWrite(SS_PIN, LOW);    
+  delayMicroseconds(1);    
+  digitalWrite(SS_PIN, HIGH);
+  digitalWrite(SS_PIN, LOW);
+  delayMicroseconds(1);    
+  digitalWrite(SS_PIN, HIGH);
+  SPI.begin();
+    
+    Serial.begin(115200);
+    #if defined(ESP32)
+      WiFi.setHostname(ssid);
+    #else
+      WiFi.hostname(ssid);
+    #endif
+     
      WiFi.begin(ssid, password);
      Serial.println("");
      // Wait for connection
@@ -176,8 +198,14 @@ void setup(void) {
      Serial.println("");
      Serial.print("IP address: ");
      Serial.println(WiFi.localIP());
-   
-
+     
+  #if defined(M5STACK)
+    M5.Lcd.setTextColor(TFT_GREEN,TFT_BLACK);
+    M5.Lcd.setTextFont(4);
+    // LCD display
+    M5.Lcd.print("\nip\n:");
+    M5.Lcd.print(WiFi.localIP()); 
+  #endif
   ESPUI.label("Status:", COLOR_TURQUOISE, "Stop");
   ESPUI.label("Millis:", COLOR_EMERALD, "0");
   ESPUI.button("Push Button", &buttonCallback, COLOR_PETERRIVER);
@@ -196,9 +224,6 @@ void setup(void) {
      If you want to serve the files from SPIFFS use ESPUI.beginSPIFFS
      (.prepareFileSystem has to be run in an empty sketch before)
    */
-
-  dnsServer.start(DNS_PORT, "*", apIP);
-
   /*
    * Optionally you can use HTTP BasicAuth. Keep in mind that this is NOT a
    SECURE way of limiting access.
@@ -206,11 +231,10 @@ void setup(void) {
    since it is transmitted in cleartext ESPUI.begin("ESPUI Control", "myuser",
    "mypassword");
   */
-  ESPUI.begin("ESPUI Control");
+  ESPUI.beginSPIFFS("Test123");
 }
 
 void loop(void) {
-  dnsServer.processNextRequest();
 
   if (millis() - oldTime > 5000) {
     ESPUI.print("Millis:", String(millis()));
